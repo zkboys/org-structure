565d972b32112c781e5b149a4561114f64ec9a56
wangshubin <wangshubin@xiaoyage.com>
2016-05-24 18:44:03 +0800

    修改404页面样式

--- assets/src/page/error/Error404.jsx
+++ assets/src/page/error/Error404.jsx
@@ -1,37 +1,18 @@
 import './style.less';
 import React from 'react';
-import { Breadcrumb, Button } from 'antd'
+import {Breadcrumb, Button} from 'antd'
 import {Link} from 'react-router'
-import createBrowserHistory from 'history/lib/createBrowserHistory'
-const browserHistory = createBrowserHistory();
-import Page from '../../framework/page/Page';
-class Error404 extends React.Component {
-    constructor(props){
-        super(props);
-    }
-    goBack() {
-        browserHistory.goBack()
-    }
-    render() {
-        let header =
-            <div>
-                <h1 className="admin-page-header-title">NotFound</h1>
-                <Breadcrumb>
-                    <Breadcrumb.Item><Link to="/">首页</Link></Breadcrumb.Item>
-                    <Breadcrumb.Item>未找到</Breadcrumb.Item>
-                </Breadcrumb>
-            </div>;
-        return (
-            <Page header={header}>
-                <div id="admin-page-header" className="admin-page-header">
+import error404 from './404.png'
 
-                </div>
-                <h1>404</h1>
-                <p>您访问的页面不存在</p>
-                <Button onClick={this.goBack}><a href="javascript:;">返回上一级</a></Button>
-                <Button> <Link to="/">返回首页</Link></Button>
-            </Page>
+export default React.createClass({
+    render() {
+        return (
+            <div className="error-wrap">
+                <img src={error404} alt="404图片"/>
+                <p className="error-text">您访问的页面不存在...</p>
+                <Button type="primary" className="error-btn" onClick={()=>{this.props.history.goBack()}}><a href="javascript:;">返回上一级</a></Button>
+                <Button type="primary" className="error-btn error-btn-right"> <Link to="/">返回首页</Link></Button>
+            </div>
         );
     }
-}
-export default Error404;
+});
