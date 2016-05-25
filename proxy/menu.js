b48631a452ed1ca5311c73206d9a72e09c04f18a
wangshubin <wangshubin@xiaoyage.com>
2016-05-25 11:06:20 +0800

    添加eslintrc支持

--- proxy/menu.js
+++ proxy/menu.js
@@ -10,19 +10,17 @@ var Menu = require('../models').Menu;
  * - docs，返回的数据
  * */
 exports.getAllMenus = function (callback) {
-    Menu.find(callback);
+  Menu.find(callback);
 };
 /**
- * 更新所有菜单
- * callback(err, jellybean, snickers):
  *
- *
- *
- * */
-exports.updateAllMenus = function (newMenus, callback) {
-    Menu.remove({}, function (err, docs) {
-        Menu.create(newMenus, callback)
-    });
+ * @param callback
+ * @param newMenu
+ */
+exports.updateAllMenus = function (newMenu, callback) {
+  Menu.remove({}, function (err, docs) {
+    Menu.create(newMenus, callback)
+  });
 }
 /**
  * 根据用户，获取此用户有权限得菜单
@@ -31,6 +29,6 @@ exports.updateAllMenus = function (newMenus, callback) {
  * - docs，返回的数据
  * */
 exports.getMenusByUser = function (user, callback) {
-    // TODO 添加权限判断
-    Menu.find(callback);
+  // TODO 添加权限判断
+  Menu.find(callback);
 };
