51c448a16797a10d488a1149e2059f791a35f2fb
wangshubin <wangshubin@xiaoyage.com>
2016-06-29 18:59:58 +0800

    扩展封装

--- assets/src/common/storage.js
+++ assets/src/common/storage.js
@@ -1,8 +1,26 @@
 /**
  *  本地存储封装，项目中其他地方不要直接使用localStorage和sessionStorage，统一使用封装。
  *  简化接口，字符串json转换。
+ *  如果本地存储使用比较多，可以考虑封装LRU（Least Recently Used最近最少使用）
  */
 export default {
+    global: {
+        get(key) {
+            return window.globalStorage ? window.globalStorage[key] : null;
+        },
+        set(key, jsonValue) {
+            window.globalStorage = window.globalStorage ? window.globalStorage : {};
+            window.globalStorage[key] = jsonValue;
+        },
+        remove(key) {
+            if (window.globalStorage) {
+                delete window.globalStorage[key];
+            }
+        },
+        removeAll() {
+            window.globalStorage = {};
+        },
+    },
     local: {
         get(key) {
             const strValue = localStorage.getItem(key);
@@ -14,6 +32,9 @@ export default {
         },
         remove(key) {
             localStorage.removeItem(key);
+        },
+        removeAll() {
+            localStorage.clear();
         },
     },
     session: {
@@ -27,6 +48,9 @@ export default {
         },
         remove(key) {
             sessionStorage.removeItem(key);
+        },
+        removeAll() {
+            sessionStorage.clear();
         },
     },
 };
